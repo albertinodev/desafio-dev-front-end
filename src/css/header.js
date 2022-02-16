26d0bbec22edaa066975e9b4c582b81421ff2190
Albertino Augusto <albertinoaugusto.it@gmail.com>
2022-02-16 21:12:47 +0200

    Header, And Tool Creation

---
+++ src/css/header.js
@@ -0,0 +1,52 @@
+const cssProperties = (): any => {
+    return {
+      searchBox: {
+        border: '3px solid black',
+        borderRadius: 3
+      },
+      searchBlock: {
+        display: 'block',
+        float: 'left'
+      },
+      searchIncon: {
+        position: 'relative',
+        top: 3,
+        left: 4
+      },
+      searchInput: {
+        top: -1,
+        position: 'relative',
+        border: 'none',
+        marginLeft: 8
+      },
+      checkBox: {
+        position: 'relative',
+        top: -24,
+        left: 210
+      },
+      checkBoxInput: {
+        backgroundColor: 'black',
+        color: 'black'
+      },
+      addButton: {
+        backgroundColor: '#ffffff',
+        boxShadow: '2px 2px 1px #222',
+        borderRadius: 2,
+        border: '2px solid black'
+      },
+      plusSign: {
+        fontSize: 20,
+        fontWeight: 'bolder',
+        position: 'relative',
+        marginRight: 2,
+        top: 3
+      },
+      addButtonBox: {
+        display: 'block',
+        float: 'right'
+      },
+    }
+  }
+  
+  export default cssProperties
+  
---
+++ src/css/tool.js
@@ -0,0 +1,36 @@
+const cssProperties = (): any => {
+    return {
+      toolBox: {
+        backgroundColor: 'white',
+        border: '3px solid black',
+        borderRadius: 3,
+        padding: 10,
+        marginTop: 24
+      },
+      toolName: { 
+          display: 'block', float: 'left'
+      },
+      removeTool: { 
+          display: 'block', 
+          float: 'right', 
+          marginBottom: 6,
+          cursor: "pointer"
+       },
+      xIcon: { 
+          fontWeight: 'bolder',
+          fontSize: 14 
+      },
+      description: { 
+        textAlign: "left",
+        marginTop: 10, 
+        marginBottom: 10,
+        marginRight: 10
+      },
+      ashTag: { 
+        fontWeight: "bolder" 
+      }
+    }
+  }
+  
+  export default cssProperties
+  
